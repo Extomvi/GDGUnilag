199c38722d14ba03842f534af0228d86f767a77a
Extomvi <exceltadedokun@gmail.com>
2020-04-07 19:29:52 +0100

    'Well'

---
+++ inPython/COURSERA/Google IT Automation/week4/classes.py
@@ -0,0 +1,15 @@
+'''
+UNDERSTANDING CLASSES, THEIR METHODS AND INSTANCES
+'''
+
+class Apples:
+    color = ""
+    flavor = ""
+
+fruit = Apples()
+fruit.color = "red"
+fruit.flavor = "sweet"
+
+veg = Apples()
+veg.color = "green"
+veg.flavor = "bleh"
