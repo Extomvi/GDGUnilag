a37c2779f2ce24dea5e41859926f26b094ffeb33
Extomvi <exceltadedokun@gmail.com>
2020-05-28 18:02:14 +0100

    Added 'Whatsapp.py' with Twilio

---
+++ inPython/Whatsapp.py
@@ -0,0 +1,12 @@
+import os
+from twilio.rest import Client
+
+client = Client()
+
+from_whatsapp_number= 'whatsapp:+14155238886'
+
+to_whatsapp_number= 'whatsapp:' + os.environ['MY_PHONE_NUMBER']
+
+client.messages.create(body='Trial',
+from_=from_whatsapp_number,
+to=to_whatsapp_number)
