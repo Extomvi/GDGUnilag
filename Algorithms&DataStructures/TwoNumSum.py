1a2e1d0bf90b80ec0138375c77218a6fbad5bbd6
Extomvi <exceltadedokun@gmail.com>
2021-01-25 23:48:34 +0100

    Two Number Sum Updated

---
+++ Algorithms&DataStructures/TwoNumSum.py
@@ -0,0 +1,38 @@
+### Two Number Sum
+def twoNumberSum(array, targetSum): //O(n^2) Operation
+    # Write your code here.
+	for items in range(len(array)):
+		first_item = array[items]
+		for num in range(items+1, len(array)):
+			second_item = array[num]
+			if first_item + second_item == targetSum:
+				return [first_item, second_item]
+	return []
+
+def twoNumberSum(array, targetSum): // O(n) Solution
+    # Write your code here.
+	nums = {}
+	for num in array:
+		target = targetSum - num
+		if target in nums:
+			return [target, num]
+		else:
+			nums[num] = True
+    return []
+
+def twoNumberSum(array, targetSum): // O(nlogn) Solution
+    # Write your code here.
+	array.sort()
+	left = 0
+	right = len(array) -1
+	while left < right:
+		result = array[left] + array[right]
+		if result == targetSum:
+			return [array[left], array[right]]
+		elif result < targetSum:
+			left += 1
+		elif result > targetSum:
+			right -= 1
+    return []
+
+    
