5e75865e190a430f9512931c794a72eb5e433706
Extomvi <exceltadedokun@gmail.com>
2021-01-26 10:30:22 +0100

    Added return for empty list

--- Algorithms&DataStructures/TwoNumberSum.py
+++ Algorithms&DataStructures/TwoNumberSum.py
@@ -6,6 +6,7 @@ def twoNumSum(array, target):
             second = a[j]
             if second + first == target:
                 print([first, second])
+    return []
     
 print(twoNumSum([2,4,5,-2,43,79,1,45,2,56,7,3,2,45,6,2,1,10,6,9], 15))     
         
