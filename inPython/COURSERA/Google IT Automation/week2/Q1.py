337a681c0cddf996348a42461eee4dd007ccc76e
Extomvi <exceltadedokun@gmail.com>
2021-03-27 22:18:37 +0100

    "Changes to settings"

--- inPython/COURSERA/Google IT Automation/week2/Q1.py
+++ inPython/COURSERA/Google IT Automation/week2/Q1.py
@@ -1,12 +1,12 @@
 def color_translator(color):
     if color == "red":
-        hex_color = "#ff0000"
+      hex_color = "#ff0000"
     elif color == "green":
-		hex_color = "#00ff00"
+		  hex_color = "#00ff00"
     elif color == "blue":
-		hex_color = "#0000ff"
+		  hex_color = "#0000ff"
     else:
-		hex_color = "unknown"
+		  hex_color = "unknown"
     return hex_color
 
 print(color_translator("blue")) # Should be #0000ff
