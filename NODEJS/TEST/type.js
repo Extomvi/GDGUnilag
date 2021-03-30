ba59de0ec2efe5c6a5091bc3be846e566926d81e
Extomvi <exceltadedokun@gmail.com>
2021-03-30 07:45:16 +0100

    js code

---
+++ NODEJS/TEST/type.js
@@ -0,0 +1,12 @@
+const age = 21;
+
+const addMyAge = age + 30;
+
+console.log(addMyAge);
+//console.log("Are you working?");
+
+const firstName = "Tomiwa";
+const lastName = "Adedokun";
+
+const fullName = ${firstName} ${lastName};
+console.log(fullName);
