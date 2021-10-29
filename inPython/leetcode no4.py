02f834863ccdbb44bb37a49f6658e2049cafb6fa
Tashy009 <shittutaofeek009@gmail.com>
2021-10-29 18:17:17 +0100

    added leetcode 4

---
+++ inPython/leetcode no4.py
@@ -0,0 +1,48 @@
+m = len(nums2)
+n = len(nums1)
+ar1 = nums1
+ar2 = nums2
+i = 0
+j = 0
+m1, m2 = -1, -1
+if((m + n) % 2 == 1):
+    for count in range(((n + m) // 2) + 1):
+        if(i != n and j != m):
+            if ar1[i] > ar2[j]:
+                m1 = ar2[j]
+                j += 1
+            else:
+                m1 = ar1[i]
+                i += 1
+        elif(i < n):
+            m1 = ar1[i]
+            i += 1
+
+    # for case when j<m,
+        else:
+            m1 = ar2[j]
+            j += 1
+    print(m1)
+
+    # median will be average of elements
+    # at index ((m + n)/2 - 1) and (m + n)/2
+    # in the array obtained after merging ar1 and ar2
+else:
+    for count in range(((n + m) // 2) + 1):
+        m2 = m1
+        if(i != n and j != m):
+            if ar1[i] > ar2[j]:
+                m1 = ar2[j]
+                j += 1
+            else:
+                m1 = ar1[i]
+                i += 1
+        elif(i < n):
+            m1 = ar1[i]
+            i += 1
+
+            # for case when j<m,
+        else:
+            m1 = ar2[j]
+            j += 1
+print((m1 + m2)/2)
