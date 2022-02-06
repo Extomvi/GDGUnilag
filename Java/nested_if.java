03aa9603ff227176e9eb108dfd60ea73da30dbf8
Extomvi <exceltadedokun@gmail.com>
2022-02-06 17:30:56 +0100

    Nested-if in Java

---
+++ Java/nested_if.java
@@ -0,0 +1,14 @@
+/* This is a Nested IF scenario */
+
+public class Test {
+    public static void main(String args[]) {
+        int x = 30;
+        int y = 10;
+
+        if (x == 30){
+            if (y == 10){
+                System.out.print("The value of x is 30 and y is 10");
+            }
+        }
+    }
+}
