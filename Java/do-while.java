7aacd6a96b27de6497a408799d949fa146884437
Extomvi <exceltadedokun@gmail.com>
2022-02-06 18:00:47 +0100

    Do-while loop

---
+++ Java/do-while.java
@@ -0,0 +1,12 @@
+/* Do-while in Java */
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 10;
+        do {
+            System.out.print("Value of x : " + x);
+            x++;
+            System.out.print("\n");
+        }while(x < 20);
+    }
+}
