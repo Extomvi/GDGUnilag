5dae33cf77822e4bf0cbbffaef2927bd18f06490
Extomvi <exceltadedokun@gmail.com>
2022-02-06 17:18:10 +0100

    If statement

---
+++ Java/if.java
@@ -0,0 +1,13 @@
+/* First Java program - If statements */
+
+public class Test{
+    public static void main(String[] args) {
+        int x = 10;
+
+        if (x < 10){
+            System.out.print("This is an if statement");
+        }
+        
+    }
+        
+}
