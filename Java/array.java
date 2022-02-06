1058ebed749943bfc119a3971f841d25c3675c22
Extomvi <exceltadedokun@gmail.com>
2022-02-06 20:11:30 +0100

    Array in java

---
+++ Java/array.java
@@ -0,0 +1,15 @@
+/* Data structures in Java - Arrays */
+
+class TestArray {
+    public static void main(String[] args) {
+        int a[] = new int[5];
+        a[0] = 10;
+        a[1] = 20;
+        a[2] = 70;
+        a[3] = 40;
+        a[4] = 50;
+        for(i = 0; i<a.length; i++){
+            System.out.println(a[i]);
+        }
+    }
+}
