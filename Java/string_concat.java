bb4ed4b364884faaeac6b306095d746aa837e47d
Extomvi <exceltadedokun@gmail.com>
2022-02-06 18:58:15 +0100

    String concatenation in Java

---
+++ Java/string_concat.java
@@ -0,0 +1,8 @@
+/* String concatenation in Java */
+
+public class StringTest {
+    public static void main(String[] args) {
+        String string1 = "saw I was ";
+        System.out.println("Dot " + string1 + "tod");
+    }
+}
