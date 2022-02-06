d8b2e980a8ef0c8fbfa86232adb7d029337f17f7
Extomvi <exceltadedokun@gmail.com>
2022-02-06 18:55:56 +0100

    String length in Java

---
+++ Java/stringLength.java
@@ -0,0 +1,9 @@
+/* String length in Java */
+
+public class StringTest {
+    public static void main(String[] args) {
+        String palindrome = "Dot saw I was tod";
+        int len = palindrome.length(); //Using the length method
+        System.out.println("String length is : " + len);
+    }
+}
