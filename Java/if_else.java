3ef08ec06084b8a71bb98d1baff35fb3689e7608
Extomvi <exceltadedokun@gmail.com>
2022-02-06 17:23:32 +0100

    If-else statement in Java

---
+++ Java/if_else.java
@@ -0,0 +1,14 @@
+/* If - Else statement */
+
+public class Test {
+    public static void main(String args[]) {
+        int x = 30;
+
+        if (x < 18) {
+            System.out.print("This is the if statement");
+        } else {
+            System.out.print("This is the else statement section");
+        }
+        
+    }
+}
