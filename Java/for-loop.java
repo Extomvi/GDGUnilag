6cbf48cbab289d7ad7b98fa82c7f03b289416a27
Extomvi <exceltadedokun@gmail.com>
2022-02-06 17:59:08 +0100

    Updated for-loop

--- Java/for-loop.java
+++ Java/for-loop.java
@@ -2,7 +2,7 @@
 
 public class Test {
     public static void main(String[] args) {
-        for(int x = 10; x < 20; x++){
+        for(int x = 10; x < 20; x = x + 1){
             System.out.print("Value of x : " + x);
             System.out.print("\n");
         }
