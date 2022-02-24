288259581a749eea31d6bc9132b8de44748c950f
Extomvi <exceltadedokun@gmail.com>
2022-02-24 22:45:54 +0100

    Reverse string

---
+++ Java/Reverse_string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void ReverseString(char[] str){
+        int len = str.length;
+        int left = 0;
+        int right  = len - 1;
+        while (left < right){
+            char tmp = str[left];
+            str[left] = str[right];
+            str[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+    
+}
