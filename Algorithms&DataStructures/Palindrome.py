3b1e8c67216e4445f857a5bd2404224bea2bd0a9
Extomvi <exceltadedokun@gmail.com>
2022-02-24 23:55:04 +0100

    Palindrome Checker

---
+++ Algorithms&DataStructures/Palindrome.py
@@ -0,0 +1,15 @@
+"""Palindrome Checker"""
+
+def is_palindrome(str): #One liner code
+    return str==''.join(str[::-1])
+
+def isPalindrome(str):
+    reversed_string = str[::-1]
+    if str == reversed_string:
+        return True
+    return False
+
+if __name__ == "__main__":
+
+    str = "madam"
+    print(isPalindrome(str))
