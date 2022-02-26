a0f377d3f29e32e14bb175cb33726accf2d89d59
Extomvi <exceltadedokun@gmail.com>
2022-02-26 03:03:17 +0100

    Duplicate number

---
+++ Algorithms&DataStructures/duplicateNum_in_array.py
@@ -0,0 +1,14 @@
+"""Finding duplicate numbers in an array"""
+
+def duplicates(nums):
+    
+    for num in nums:
+        if nums[abs(num)] >= 0:
+            nums[abs(num)] = -nums[abs(num)]
+        else:
+            print("Repeatition found: ", abs(num))
+
+
+if __name__ == "__main__":
+    nums = [2,3,4,1,3,6, 2]
+    duplicates(nums)
