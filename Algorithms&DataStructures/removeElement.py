81b4df98c6163d7d08ab5efaa3c25e2ab6542712
Extomvi <exceltadedokun@gmail.com>
2022-03-04 00:02:26 +0100

    Update to Remove Element

--- Algorithms&DataStructures/removeElement.py
+++ Algorithms&DataStructures/removeElement.py
@@ -6,7 +6,7 @@ def removeElement(nums, val):
 
     for i in range(len(nums)):
         if nums[i] != val:
-            nums[i] = nums[k]
+            nums[k] = nums[i]
             k += 1
     return k
 
