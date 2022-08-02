51385d79a08bf5b37c2497155a4f1689b3023cac
Tomiwa <exceltadedokun@gmail.com>
2022-08-02 10:11:45 +0000

    Create dfs.py file

---
+++ Algorithms&DataStructures/dfs.py
@@ -0,0 +1,22 @@
+"""
+Recursive traversal implementation of depth first search algorithm
+"""
+#Recursive traversal method
+graph = {
+    'A': ['B', 'C', 'D'],
+    'B': ['E'],
+    'C': ['D', 'E'],
+    'D': [],
+    'E': []
+}
+
+visited = set()
+
+def dfs(visited, graph, root):
+    if root not in visited:
+        print(root)
+        visited.add(root)
+        print(visited)
+        for next in graph[root]:
+            dfs(visited, graph, next) #recursive call
+dfs(visited, graph, 'A')
